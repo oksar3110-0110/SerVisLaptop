199d8d4b3b2282560dcdab8ec7cb42ce3d44d087
oksar3110-0110 <oksareinaldi@gmail.com>
2018-04-24 08:24:23 +0700

    Commit Perdana plus Upload praktikum 1-8 belum fix

---
+++ app/src/main/java/com/wordpress/oksareinaldi/servislaptop/ListCabang.java
@@ -0,0 +1,18 @@
+package com.wordpress.oksareinaldi.servislaptop;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListCabang extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_list_cabang, null);
+    }
+}
